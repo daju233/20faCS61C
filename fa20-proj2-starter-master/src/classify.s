e7dcae4b4d0074e5bc28c62e9c1321d08d6ad467
DA-JU <2212509690@qq.com>
2024-08-13 04:55:31 +0800

    finish write_matrix

--- fa20-proj2-starter-master/src/classify.s
+++ fa20-proj2-starter-master/src/classify.s
@@ -22,6 +22,19 @@ classify:
     #   main.s <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH>
 
 
+    addi sp sp -48
+    sw ra 0(sp)
+    sw s0 4(sp)
+    sw s1 8(sp)
+    sw s2 12(sp)
+    sw s3 16(sp)
+    sw s4 20(sp)
+    sw s5 24(sp)
+    sw s6 28(sp)
+    sw s7 32(sp)
+    sw s8 36(sp)
+    sw s9 40(sp)
+    sw s10 44(sp)
 
 
 
@@ -29,8 +42,10 @@ classify:
 	# =====================================
     # LOAD MATRICES
     # =====================================
-
-
+    sw s0 a0
+    sw s1 a1
+    sw s2 a2
+    
 
 
 
@@ -106,3 +121,36 @@ classify:
 
 
     ret
+
+    lw s7 32(sp)
+    lw s6 28(sp)
+    lw s5 24(sp)
+    lw s4 20(sp)
+    lw s3 16(sp)
+    lw s2 12(sp)
+    lw s1 8(sp)
+    lw s0 4(sp)
+    lw ra 0(sp)
+
+    addi sp sp 36
+    ret
+
+exit_88:
+
+    li a1 88
+    jal exit2
+
+exit_90:
+
+    li a1 90
+    jal exit2
+
+exit_91:
+
+    li a1 91
+    jal exit2
+
+exit_92:
+
+    li a1 92
+    jal exit2
--- fa20-proj2-starter-master/src/write_matrix.s
+++ fa20-proj2-starter-master/src/write_matrix.s
@@ -25,16 +25,91 @@
 write_matrix:
 
     # Prologue
+    addi sp sp -24
+    sw ra 0(sp)
+    sw s0 4(sp)
+    sw s1 8(sp)
+    sw s2 12(sp)
+    sw s3 16(sp)
+    sw s4 20(sp)
 
 
+    mv s0 a0 #filename
+    mv s1 a1 #start of matrix
+    mv s2 a2 #rows in the matrix
+    mv s3 a3 #columns of matrix
+
+    mv a1 s0
+    li a2 1
+    jal fopen
+    li t0 -1
+    beq a0 t0 exit_93
+    mv s4 a0 #fd
+
+    #allocate buffer 
+    li a0 8
+    jal malloc
+    #beq x0 a0 exit_88
+
+    #store the data to buffer
+    sw s2 0(a0)
+    sw s3 4(a0)
+
+    #write rows and cols
+    mv a1 s4
+    mv a2 a0
+    li a3 2
+    li a4 4
+    jal fwrite
+    li t0 2
+    bne a0 t0 exit_94
 
 
+    #write matrix
+    mv a1 s4
+    mv a2 s1
+    mul a3 s2 s3
+    li a4 4
+    jal fwrite
+    mul t0 s2 s3
+    bne a0 t0 exit_94
+
+    mv a1 s4
+    jal fclose
+    li t0 -1
+    beq t0 a0 exit_95
+
+    lw s4 20(sp)
+    lw s3 16(sp)
+    lw s2 12(sp)
+    lw s1 8(sp)
+    lw s0 4(sp)
+    lw ra 0(sp)
+
+    addi sp sp 24
+    ret
+
+# exit_88:
+
+#     li a1 88
+#     jal exit2
 
 
+exit_93:
 
+    li a1 93
+    jal exit2
 
+exit_94:
+
+    li a1 94
+    jal exit2
+
+exit_95:
+
+    li a1 95
+    jal exit2
 
     # Epilogue
 
 
-    ret
